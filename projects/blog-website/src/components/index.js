7665ae485a1f5e829ac353a14a65859ff2e869d6
Vikas <vikasraivsr630@gmail.com>
2024-03-09 11:16:42 +0530

    added frontend

--- projects/blog-website/src/components/index.js
+++ projects/blog-website/src/components/index.js
@@ -1,7 +1,31 @@
 import Header from "./Header/Header.jsx"
 import Footer from "./Footer/Footer.jsx"
+import Container from "./container/Container.jsx"
+import Logo from "./Logo.jsx"
+import LogoutBtn from "./Header/LogoutBtn.jsx"
+import Button from "./Button.jsx"
+import Input from "./Input.jsx"
+import Select from "./Select.jsx"
+import PostCard from "./PostCard.jsx"
+import Signup from "./Signup.jsx"
+import AuthLayout from "./AuthLauout.jsx"
+import RTE from "./RTE.jsx"
+import PostForm from "./post-form/PostForm.jsx"
+import Login from "./Login.jsx"
 
 export {
     Header,
-    Footer
+    Footer,
+    Container,
+    Logo,
+    LogoutBtn,
+    Button,
+    Input,
+    Select,
+    PostCard,
+    Signup,
+    Login,
+    AuthLayout,
+    RTE,
+    PostForm
 }
